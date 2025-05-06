da14fea61b416b5732e553ca6d77521cebb4f510
youssaf mohamed <youssafmohamed2002@gmail.com>
2025-05-07 02:39:08 +0300

    Add style to dashboard

--- test/Main.cpp
+++ test/Main.cpp
@@ -5,9 +5,9 @@ using namespace System;
 using namespace System::Windows::Forms;
 [STAThread]
 void main(array<String^>^ args) {
-	// use only if you want have changed the db structure
+	 //use only if you want have changed the db structure
 	//DBHelper::CreateDatabaseIfNotExists("pharmacy.db");
-	// dummy data for the products
+	// //dummy data for the products
 	//ProductDBHelper::InsertDummyData();
 	Application::EnableVisualStyles();
 	Application::SetCompatibleTextRenderingDefault(false);
